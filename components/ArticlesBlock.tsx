bb933c843df49e69fc8c9ea1aa02f0c1ef954f80
Mike Beasley <mbeasley4@gmail.com>
2026-03-02 10:14:37 -0500

    update to look at created at for sananty posts

--- components/ArticlesBlock.tsx
+++ components/ArticlesBlock.tsx
@@ -5,7 +5,7 @@ import { client } from "@/sanity/lib/client";
 import { urlFor } from "@/sanity/lib/image";
 import { SanityDocument } from "next-sanity";
 
-const POSTS_QUERY = `*[_type == "post" && defined(slug.current)] | order(publishedAt desc) [0...3] {
+const POSTS_QUERY = `*[_type == "post" && defined(slug.current)] | order(coalesce(publishedAt, _createdAt) desc) [0...3] {
   _id,
   title,
   slug,
