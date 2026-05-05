8ec326905a7a1619532e494edd810621cb67634b
Mike Beasley <mbeasley4@gmail.com>
2026-05-05 07:30:12 -0400

    adding speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import AptabaseClientProvider from "@/providers/AptabaseClientProvider";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({
   children,
@@ -18,6 +19,7 @@ export default function RootLayout({
           <main>{children}</main>
           <Footer />
           <Analytics />
+          <SpeedInsights />
         </AptabaseClientProvider>
       </body>
     </html>
