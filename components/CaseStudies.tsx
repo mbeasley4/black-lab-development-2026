1d292c94737076b1cc0e1615d0ea0ff7687df81b
Mike Beasley <mbeasley4@gmail.com>
2026-07-04 12:14:54 -0400

    fix: adjust Case Studies grid layout to display three columns on larger screens

--- components/CaseStudies.tsx
+++ components/CaseStudies.tsx
@@ -100,7 +100,7 @@ export default function CaseStudies() {
                 </div>
 
                 {/* Case Studies Grid */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                     {caseStudies.map((project) => {
                         const colors = colorClasses[project.color];
                         return (
